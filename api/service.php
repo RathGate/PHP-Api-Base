9e01b3297a13ddd5e5123fcb09cc2803d01212a8
RathGate <mariannecorbel@hotmail.fr>
2023-10-19 13:20:07 +0200

    tentative de factorisation

--- api/service.php
+++ api/service.php
@@ -1,9 +1,48 @@
 <?php
 abstract class Service {
+    protected $allowedVerbs = [];
+    protected $requiredParams = [];
+    protected $params;
+
     public function __construct()
     {
+        // Vérifie le verbe de la requête
+        if (!self::IsValidMethod()) {
+            $response = ApiLib::errorResponse(405, "Méthode ".$_SERVER["REQUEST_METHOD"]." non autorisée.");
+            header("HTTP/1.0 405 Method Not Allowed");
+            echo stripslashes(json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+            exit;
+        }
+
+        // Récupère, traite et vérifie les paramètres
+        $this::SetParameters();
+        // Todo: séparé de SetParameters mais peut certainement être factorisé en une fonction.
+        $this->CheckParameters();
+        // Si aucune erreur n'a été détectée, lance l'exécution du service en lui-même.
         $this->Trig();
     }
+
     abstract function Trig();
+
+    public function IsValidMethod(): bool
+    {
+        return in_array($_SERVER["REQUEST_METHOD"], $this->allowedVerbs);
+    }
+
+    // Enregistre les paramètres dans l'object $this->params.
+    public function SetParameters(): void {
+        $this->params = new stdClass();
+        foreach ($this->requiredParams as $param) {
+            if (!isset($_GET[$param])) {
+                $response = ApiLib::errorResponse(405, "Paramètre obligatoire `".$param."` manquant.");
+                header("HTTP/1.0 400 Bad Request");
+                echo stripslashes(json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+                exit;
+            }
+            $this->params->$param = json_decode($_GET[$param]);
+        }
+    }
+    public abstract function CheckParameters();
+
 }
 
--- api/sort/sortService.php
+++ api/sort/sortService.php
@@ -3,47 +3,57 @@
 require_once __DIR__ . "/../../libs/sortLib.php";
 require_once __DIR__ . "/../../libs/apiLib.php";
 require_once __DIR__."/../service.php";
-class SortService extends Service {
 
-    // Propriétés propres à l'endpoint service (type de tri et tableau à trier)
-    private $sortFunc;
-    private $queryArr;
+class SortService extends Service {
 
     // Surcharge Service.__construct() pour ajouter le traitement spécifique de la requête.
     public function __construct()
     {
-        // Récupère le nom de l'endpoint (.../{endpoint}/index.php)
-        preg_match("/^.*\/(?P<folder_name>.+)\/.+\.php$/", $_SERVER["PHP_SELF"], $matches);
-
-        // Vérifie si une fonction du nom récupéré {endpoint} existe dans SortLib, sinon attribue 'false'
-        // Todo: possible de retourner une erreur dès le constructeur ?
-        $this->sortFunc = method_exists(SortLib::class, $matches["folder_name"] ?? false) ?
-            $matches["folder_name"] : false;
-        $this->queryArr = json_decode($_GET["arr"] ?? false);
-
-        // Appelle le constructeur du parent
-        parent::__construct();
+        $this->allowedVerbs = ["GET"];
+        $this->requiredParams = ["arr"];
+        parent::__construct();;
     }
 
     function Trig(): void
     {
-        // Vérification de base des attributs et du verbe de la requête
-        if ($_SERVER["REQUEST_METHOD"] !== "GET" || !$this->queryArr || !is_array($this->queryArr)) {
-            header("HTTP/1.0 400 Bad Request");
-            return;
-        }
-        // Aucune fonction associée à la route
-        if (!$this->sortFunc) {
-            header("HTTP/1.0 500 Internal Server Error");
-            return;
-        }
+        // Trie l'array avec la fonction associée à l'endpoint
+        $sortedArr = SortLib::{$this->params->sortFunc}($this->params->arr);
 
-        // Trie l'array et le renvoie
-        $sortedArr = SortLib::{$this->sortFunc}($this->queryArr);
-        $response = ApiLib::successResponse(array("sort_function"=>$this->sortFunc, "sorted_arr"=>$sortedArr));
+        // Ecrit le json de la réponse et l'envoie
+        $response = ApiLib::successResponse(array("sort_function"=>$this->params->sortFunc, "sorted_arr"=>$sortedArr));
         header('Content-Type: application/json');
 
-        // Renvoie la réponse, succès ou échec
         echo stripslashes(json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+    }
+
+    // Surcharge Service::SetParameters() pour y ajouter le nom de la fonction de tri à utiliser,
+    // récupérée depuis l'URI de la requête.
+    public function SetParameters(): void
+    {
+        parent::SetParameters();
+
+        // Récupère le nom de l'endpoint (.../{endpoint}/index.php)
+        preg_match("/^.*\/(?P<folder_name>.+)\/.+\.php$/", $_SERVER["PHP_SELF"], $matches);
+        // Vérifie si une fonction du nom récupéré {endpoint} existe dans SortLib, sinon attribue 'false'
+        $this->params->sortFunc = $matches["folder_name"] ?? false;
+    }
+
+    // Renvoie l'erreur en réponse et termine le script si un paramètre est invalide.
+    public function CheckParameters()
+    {
+        // La méthode associée à l'endpoint n'existe pas
+        if (!method_exists(SortLib::class, $this->params->sortFunc)) {
+            $response = ApiLib::errorResponse(500, "Aucune fonction de tri associée à `".$this->params->sortFunc."`.");
+            header("HTTP/1.0 500 Internal Server Error");
+            echo stripslashes(json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+            exit;
+        }
+        // Le type du paramètre est invalide
+        if (!is_array($this->params->arr)) {
+            $response = ApiLib::errorResponse(400, "Le paramètre `arr` doit être de type array.");
+            header("HTTP/1.0 400 Bad Request");
+            echo stripslashes(json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+            exit;
+        }
     }
 }
