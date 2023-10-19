fffd0352e7ddb00e3cfe73db7f63d52713dee01f
RathGate <mariannecorbel@hotmail.fr>
2023-10-19 11:10:30 +0200

    save before checkout

--- api/sort/sortService.php
+++ api/sort/sortService.php
@@ -1,12 +1,11 @@
 <?php
 
-require_once __DIR__ . "/../../lib/sortLib.php";
-require_once __DIR__."/../../lib/apiLib.php";
+require_once __DIR__ . "/../../libs/sortLib.php";
+require_once __DIR__ . "/../../libs/apiLib.php";
 require_once __DIR__."/../service.php";
 class SortService extends Service {
 
     // Propriétés propres à l'endpoint service (type de tri et tableau à trier)
-    // Todo: Pourquoi ça plante quand j'essaie d'attribuer un type à ces attributs ?
     private $sortFunc;
     private $queryArr;
 
@@ -17,6 +16,7 @@ class SortService extends Service {
         preg_match("/^.*\/(?P<folder_name>.+)\/.+\.php$/", $_SERVER["PHP_SELF"], $matches);
 
         // Vérifie si une fonction du nom récupéré {endpoint} existe dans SortLib, sinon attribue 'false'
+        // Todo: possible de retourner une erreur dès le constructeur ?
         $this->sortFunc = method_exists(SortLib::class, $matches["folder_name"] ?? false) ?
             $matches["folder_name"] : false;
         $this->queryArr = json_decode($_GET["arr"] ?? false);
@@ -38,17 +38,10 @@ class SortService extends Service {
             return;
         }
 
-        // Protection supplémentaire dans le cas où la fonction associée à l'endpoint
-        // n'existe pas dans sortLib et lancerait une erreur.
-        // Todo: Préciser l'exception au lieu de faire un catch généraliste ? Ou pas ?
-        try {
-            $sortedArr = SortLib::{$this->sortFunc}($this->queryArr);
-            $response = ApiLib::successResponse(array("sort_function"=>$this->sortFunc, "sorted_arr"=>$sortedArr));
-            header('Content-Type: application/json');
-        } catch (Exception $e) {
-            $response = ApiLib::errorResponse(400, $e);
-            header("HTTP/1.0 400 Bad Request");
-        }
+        // Trie l'array et le renvoie
+        $sortedArr = SortLib::{$this->sortFunc}($this->queryArr);
+        $response = ApiLib::successResponse(array("sort_function"=>$this->sortFunc, "sorted_arr"=>$sortedArr));
+        header('Content-Type: application/json');
 
         // Renvoie la réponse, succès ou échec
         echo stripslashes(json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
